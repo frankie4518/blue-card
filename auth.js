ad2e4d988a6c9584422226a5535da841807c587f
frankie4518 <frankie@bloxflow.uk>
2026-04-01 21:44:23 +0100

    Refactor authentication flow in auth.js
    Refactor login function to use constants for tenant and client IDs, improve redirect URI handling, and add token parsing functionality.

--- auth.js
+++ auth.js
@@ -1,15 +1,36 @@
+const TENANT_ID = "7746fb37-f9d1-4d28-aa55-09bf3849365e";
+const CLIENT_ID = "YOUR_CLIENT_ID_HERE"; // from your app registration
+
 function login() {
-  window.location.href =
-    "https://login.microsoftonline.com/common/oauth2/v2.0/authorize" +
-    "?client_id=YOUR_CLIENT_ID" +
+  const redirectUri = window.location.origin + "/staff.html";
+  const authUrl =
+    "https://login.microsoftonline.com/" + TENANT_ID + "/oauth2/v2.0/authorize" +
+    "?client_id=" + encodeURIComponent(CLIENT_ID) +
     "&response_type=token" +
-    "&redirect_uri=" + encodeURIComponent(window.location.href) +
-    "&scope=openid%20email%20profile" +
-    "&domain_hint=blue-card.co.uk";
+    "&redirect_uri=" + encodeURIComponent(redirectUri) +
+    "&scope=" + encodeURIComponent("openid profile email") +
+    "&prompt=select_account";
+
+  window.location.href = authUrl;
+}
+
+function parseHashToken() {
+  if (window.location.hash.startsWith("#")) {
+    const params = new URLSearchParams(window.location.hash.substring(1));
+    const accessToken = params.get("access_token");
+    if (accessToken) {
+      sessionStorage.setItem("ms_access_token", accessToken);
+      // clean hash
+      window.location.hash = "";
+    }
+  }
 }
 
 function requireLogin() {
-  if (!localStorage.getItem("ms_token")) {
+  parseHashToken();
+  const token = sessionStorage.getItem("ms_access_token");
+  if (!token) {
     window.location.href = "index.html";
   }
+  return token;
 }
